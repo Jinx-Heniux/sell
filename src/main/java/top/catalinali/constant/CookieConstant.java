0e5dcd18b8a805318b19e04b1c2917ffd915c2b7
catalinaLi <392235464@qq.com>
2018-05-21 10:50:14 +0800

    add login

---
+++ src/main/java/top/catalinali/constant/CookieConstant.java
@@ -0,0 +1,15 @@
+package top.catalinali.constant;
+
+/**
+ * @Author: lx
+ * @Description: cookie常量
+ * @Date: Created on 10:40 2018/5/21
+ * @Modefied by:
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+
+}
---
+++ src/main/java/top/catalinali/constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package top.catalinali.constant;
+
+/**
+ * @Author: lx
+ * @Description: redis常量
+ * @Date: Created on 10:42 2018/5/21
+ * @Modefied by:
+ */
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+
+    Integer EXPIRE = 7200; //2小时
+}
+
